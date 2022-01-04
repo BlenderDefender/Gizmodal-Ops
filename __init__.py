c0734a46bdaa465059da7ebac6f2c2d7edc132ed
Blender Defender <defenderblender@gmail.com>
2022-01-04 11:27:46 +0100

    Initialize the addon.

---
+++ __init__.py
@@ -0,0 +1,41 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  <An addon to blend Gizmo and Modal operations more seamlessly>
+#    Copyright (C) <2022>  <Mat Brady>
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+bl_info = {
+    "name": "Gizmodal Ops",
+    "author": "Mat Brady, Blender Defender",
+    "version": (1, 0, 0),
+    "blender": (2, 83, 0),
+    "location": "3D View > G, R or S",
+    "description": "An addon that blends Gizmo and Modal operations more seamlessly",
+    "warning": "",
+    "wiki_url": "https://github.com/BlenderDefender/Gizmodal-Ops#gizmodal-ops",
+    "tracker_url": "https://github.com/BlenderDefender/Gizmodal-Ops/issues",
+    "category": "3D View"
+}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
