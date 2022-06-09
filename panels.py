464d62ee6b839a00e6e3b63204ee54d230017010
Blender Defender <defenderblender@gmail.com>
2022-06-09 10:28:57 +0200

    feat(UI): Improve the layout of the preferences panel.
    * Adjust the spacing between lines of text. * Adjust the spacing between items * Display a reminder to save the preferences, when auto_save_preferences is turned of.

--- panels.py
+++ panels.py
@@ -38,56 +38,80 @@ class GIZMODAL_OPS_PT_Panel(Panel):
         layout = self.layout
         prefs = context.preferences.addons[__package__].preferences
 
+        SPACING = 0.8
+
         # Add a sublayout for all properties that are dependent on auto_lock_to_view.
         numeric_props_col = layout.column()
         numeric_props_col.enabled = not prefs.auto_lock_to_view
 
-        seconds_row = numeric_props_col.row()
-        seconds_col = seconds_row.column()
-
         # Create a row for a label.
-        label_row = seconds_col.row()
-        label_row.label(
+        label_col = numeric_props_col.column()
+        label_col.scale_y = SPACING
+        label_col.label(
             text="Listen for Modal Operations")
-        label_row = seconds_col.row()
-        label_row.label(
+        label_col.label(
             text="after pressing G / R / S for:")
 
-        # Display the time_window prop.
-        prop_row = seconds_col.row()
+        numeric_props_col.separator(factor=1)  # Insert horizontal space.
+
+        # Display the time_window property.
+        prop_row = numeric_props_col.row()
         prop_row.alignment = "RIGHT"
         prop_row.label(text="Seconds")
         prop_row.prop(prefs, "time_window", text="")
 
-        # Create a row for a label.
-        label_row = numeric_props_col.row()
-        label_row.label(
+        numeric_props_col.separator(factor=2.5)  # Insert horizontal space.
+
+        # Create a column for a label.
+        label_col = numeric_props_col.column()
+        label_col.scale_y = SPACING
+        label_col.label(text="────────────────────────────")
+        label_col.label(
             text="Hold G / R / S + move mouse")
-        label_row = numeric_props_col.row()
-        label_row.label(
+        label_col.label(
             text="to trigger Lock to View Axis")
-        label_row = numeric_props_col.row()
-        label_row.label(
+        label_col.label(text="────────────────────────────")
+
+        label_col.separator(factor=SPACING)  # Insert horizontal space.
+
+        label_col.label(
             text="Mouse Sensitivity during hold:")
 
-        # Display the mouse_sensitivity prop.
+        numeric_props_col.separator(factor=1)  # Insert horizontal space.
+
+        # Display the mouse_sensitivity property.
         prop_row = numeric_props_col.row()
         prop_row.alignment = "RIGHT"
         prop_row.label(text="0 - 10")
         prop_row.prop(prefs, "mouse_sensitivity", text="")
 
-        # Create a row for a label.
-        label_row = layout.row()
-        label_row.label(
+        numeric_props_col.separator(factor=2.5)  # Insert horizontal space.
+
+        # Create a column for a label.
+        label_col = layout.column()
+        label_col.scale_y = SPACING
+        label_col.label(
             text="Switch to Lock to View Axis")
-        label_row = layout.row()
-        label_row.label(
+        label_col.label(
             text="when pressing G / R / S")
 
+        numeric_props_col.separator(factor=1)  # Insert horizontal space.
+
+        # Display the auto_lock_to_view property.
         prop_row = layout.row()
         prop_row.alignment = "RIGHT"
         prop_row.prop(prefs, "auto_lock_to_view")
-        prop_row.label(text="")
+
+        # Display a message, when auto save preferences is turned off.
+        if not context.preferences.use_preferences_save:
+            layout.separator(factor=2.5)  # Insert horizontal space.
+
+            warning_col = layout.column()
+            warning_col.enabled = False
+            warning_col.scale_y = SPACING
+            warning_col.label(text="Remember to save your", icon="ERROR")
+            warning_col.label(text="preferences after changing them.")
+            warning_col.label(text="Edit > Preferences > Save Preferences")
 
 
 classes = (
